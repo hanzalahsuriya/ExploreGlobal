e03b2e85e280bad2174092a07c755df3585c31a1
hanzalahsuriya <hanzalah@gmail.com>
2013-12-03 16:06:18 +0000

    Solution Update

---
+++ src/ExploreGobal.Presentation.ModelView/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExploreGobal.Presentation.ModelView
+{
+    public class Class1
+    {
+    }
+}
