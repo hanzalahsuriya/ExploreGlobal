ea04174e3a649c927666135935877bc1226a3fda
hanzalahsuriya <hanzalah@gmail.com>
2013-12-05 00:33:44 +0000

    log4net

---
+++ src/ExploreGlobal.Infrastructure.DependencyResolution/LoggingModule.cs
@@ -0,0 +1,21 @@
+﻿using ExploreGlobal.Infrastructure.Data;
+using ExploreGlobal.Infrastructure.Data.Repositories;
+using ExploreGlobal.Infrastructure.Interfaces;
+using ExploreGlobal.Infrastructure.Logging;
+using ExploreGobal.Business.Domain.Interfaces;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExploreGlobal.Infrastructure.DependencyResolution
+{
+    public class LoggingModule : NinjectModule 
+    {
+        public override void Load()
+        {
+            Bind<ILoggingService>().To<Log4NetLoggerService>();
+        }
+    }
+}
---
+++ src/ExploreGobal.Presentation.WebApplication/App_Start/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using ExploreGlobal.Infrastructure.DependencyResolution;
+using Ninject;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExploreGobal.Presentation.UI.App_Start
+{
+    public class NinjectDependencyResolver : IDependencyResolver 
+    {
+        public IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            // add more ninject module
+            var modules = new INinjectModule[]
+                              {
+                                 new RepositoryModule(), 
+                                 new LoggingModule()
+                              };
+
+            kernel = new StandardKernel(modules);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
