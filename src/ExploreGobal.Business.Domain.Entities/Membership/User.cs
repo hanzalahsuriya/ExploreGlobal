186e9767d1a6710dfb5b246210b2803df77bde2a
hanzalahsuriya <hanzalah@gmail.com>
2013-12-09 03:17:23 +0000

    user class

--- src/ExploreGobal.Business.Domain.Entities/Membership/User.cs
+++ src/ExploreGobal.Business.Domain.Entities/Membership/User.cs
@@ -10,5 +10,9 @@ namespace ExploreGobal.Business.Domain.Entities.Membership
         public Guid UserId { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
     }
 }
